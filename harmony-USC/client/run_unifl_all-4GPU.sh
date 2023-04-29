a1a9f92cf4090404e030e8140556a8167c7263bb
Xiaomin OUYANG <ox019@ie.cuhk.edu.hk>
2023-04-29 20:05:58 +0800

    Add files via upload

---
+++ harmony-USC/client/run_unifl_all-4GPU.sh
@@ -0,0 +1,19 @@
+CUDA_VISIBLE_DEVICES=0 python3 ./main_unimodal.py --usr_id 0 --local_modality acc &
+CUDA_VISIBLE_DEVICES=1 python3 ./main_unimodal.py --usr_id 1 --local_modality acc &
+CUDA_VISIBLE_DEVICES=2 python3 ./main_unimodal.py --usr_id 2 --local_modality acc &
+CUDA_VISIBLE_DEVICES=3 python3 ./main_unimodal.py --usr_id 3 --local_modality acc &
+CUDA_VISIBLE_DEVICES=0 python3 ./main_unimodal.py --usr_id 0 --local_modality gyr &
+CUDA_VISIBLE_DEVICES=1 python3 ./main_unimodal.py --usr_id 1 --local_modality gyr &
+CUDA_VISIBLE_DEVICES=2 python3 ./main_unimodal.py --usr_id 2 --local_modality gyr &
+CUDA_VISIBLE_DEVICES=3 python3 ./main_unimodal.py --usr_id 3 --local_modality gyr &
+CUDA_VISIBLE_DEVICES=0 python3 ./main_unimodal.py --usr_id 4 --local_modality acc &
+CUDA_VISIBLE_DEVICES=1 python3 ./main_unimodal.py --usr_id 5 --local_modality acc &
+CUDA_VISIBLE_DEVICES=2 python3 ./main_unimodal.py --usr_id 6 --local_modality acc &
+CUDA_VISIBLE_DEVICES=3 python3 ./main_unimodal.py --usr_id 7 --local_modality acc &
+CUDA_VISIBLE_DEVICES=0 python3 ./main_unimodal.py --usr_id 8 --local_modality acc &
+CUDA_VISIBLE_DEVICES=1 python3 ./main_unimodal.py --usr_id 9 --local_modality gyr &
+CUDA_VISIBLE_DEVICES=2 python3 ./main_unimodal.py --usr_id 10 --local_modality gyr &
+CUDA_VISIBLE_DEVICES=3 python3 ./main_unimodal.py --usr_id 11 --local_modality gyr &
+CUDA_VISIBLE_DEVICES=0 python3 ./main_unimodal.py --usr_id 12 --local_modality gyr &
+CUDA_VISIBLE_DEVICES=1 python3 ./main_unimodal.py --usr_id 13 --local_modality gyr &
+
